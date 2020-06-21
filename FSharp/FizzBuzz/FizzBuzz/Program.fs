ac7ca7747ad8788a56f8b5bd1929ccb44c3e3c93
Michael Oyer <code@michaeloyer.com>
2020-06-21 14:18:59 -0400

    Add FSharp FizzBuzz Example

---
+++ FSharp/FizzBuzz/FizzBuzz/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let fizzbuzz number = 
+        let modWord divisor word = if number % divisor = 0 then word else ""
+        match
+            (modWord 3 "Fizz") +
+            (modWord 5 "Buzz")
+            with 
+                | "" -> number.ToString()
+                | value -> value
+
+    let printFizzBuzz = printfn "%s" << fizzbuzz
+
+    //Option 1
+    for number in [1..100] do printFizzBuzz number 
+    //Option 2
+    Seq.iter printFizzBuzz [1..100]
+    //Option 3
+    [1..100] |> Seq.iter printFizzBuzz
+
+    0
