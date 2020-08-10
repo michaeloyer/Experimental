c8f46c6f1fbf900c10abb7bbd5470dbb3c2857b0
Michael Oyer <code@michaeloyer.com>
2020-08-09 23:39:07 -0400

    Setup calling a database in F# with Dapper

---
+++ FSharp/Databases/Databases/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Data.SQLite
+open Dapper
+
+[<CLIMutable>]
+type Customer = {
+    ID:int
+    Name:string
+}
+
+[<EntryPoint>]
+let main _ =
+    let connection = new SQLiteConnection("DataSource=:memory:;Version=3")
+    connection.Open() |> ignore
+    connection.Execute("CREATE TABLE customer (ID INTEGER PRIMARY KEY, Name Text)") |> ignore
+    connection.Execute("INSERT INTO customer (Name) VALUES ('Alice')") |> ignore
+    connection.Execute("INSERT INTO customer (Name) VALUES ('Bob')") |> ignore
+
+    connection.Query<Customer>("SELECT * FROM customer")
+    |> Seq.iter (fun { ID = id; Name = name } -> printfn "ID: %i; Name: %s" id name)
+    
+    0 
