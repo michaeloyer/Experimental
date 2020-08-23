513a411b7016ce6800fb840c957682822838f3af
Michael Oyer <code@michaeloyer.com>
2020-08-22 22:27:23 -0400

    Simplify FizzBuzz

--- FSharp/FizzBuzz/FizzBuzz/Program.fs
+++ FSharp/FizzBuzz/FizzBuzz/Program.fs
@@ -1,23 +1,22 @@
-﻿open System
+﻿[<EntryPoint>]
+let main _ =
+    let rules = 
+        [
+            (3, "Fizz")
+            (5, "Buzz")
+        ]
 
-[<EntryPoint>]
-let main argv =
     let fizzbuzz number = 
-        let modWord divisor word = if number % divisor = 0 then word else ""
-        match
-            (modWord 3 "Fizz") +
-            (modWord 5 "Buzz")
-            with 
-                | "" -> number.ToString()
-                | value -> value
+        rules
+        |> Seq.map (fun (divisor, word) -> if number % divisor = 0 then word else "")
+        |> String.concat ""
+        |> function
+            | "" -> string number
+            | value -> value
 
-    let printFizzBuzz = printfn "%s" << fizzbuzz
-
-    //Option 1
-    for number in [1..100] do printFizzBuzz number 
-    //Option 2
-    Seq.iter printFizzBuzz [1..100]
-    //Option 3
-    [1..100] |> Seq.iter printFizzBuzz
+    { 1..100 } 
+    |> Seq.map fizzbuzz
+    |> String.concat "\n"
+    |> printfn "%s"
 
     0
