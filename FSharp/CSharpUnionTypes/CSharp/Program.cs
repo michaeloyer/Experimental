800d02d2136381d0c759ed8303422272ecde5227
Michael Oyer <code@michaeloyer.com>
2020-08-23 15:43:32 -0400

    F# Discriminated Unions in C# Switch

---
+++ FSharp/CSharpUnionTypes/CSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using FSharp;
+using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDaUnion(DaUnion.NewText("Test"));
+            PrintDaUnion(DaUnion.NewText2("Test2"));
+            PrintDaUnion(DaUnion.NewPoint(100, 200));
+            PrintDaUnion(DaUnion.NewNumber(300));
+        }
+
+        static void PrintDaUnion(DaUnion daUnion)
+        {
+            switch (daUnion)
+            {
+                case DaUnion.Text { Item: string text }:
+                    Console.WriteLine($"DaUnion.Text: {text}");
+                    break;
+
+                case DaUnion.Point { Item1: int p1, Item2: int p2 }:
+                    Console.WriteLine($"DaUnion.Point: P1: {p1}; P2: {p2}");
+                    break;
+
+                case DaUnion.Number { Item: int number }:
+                    Console.WriteLine($"DaUnion.Number: {number}");
+                    break;
+
+                case DaUnion.Text2 { Item: string text }:
+                    Console.WriteLine($"DaUnion.Text2: {text}");
+                    break;
+            }
+        }
+    }
+}
---
+++ FSharp/CSharpUnionTypes/FSharp/DaUnion.fs
@@ -0,0 +1,7 @@
+﻿namespace FSharp
+
+type DaUnion =
+    | Number of int
+    | Point of int * int
+    | Text of string
+    | Text2 of string
