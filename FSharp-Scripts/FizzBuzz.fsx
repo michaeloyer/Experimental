b131c8c12a7ca0febcd89e03abf25c1b1d50ebf5
Michael Oyer <code@michaeloyer.com>
2021-11-18 22:07:58 -0500

    Move some single file F# projects to script files

---
+++ FSharp-Scripts/FizzBuzz.fsx
@@ -0,0 +1,16 @@
+﻿let rules =
+    [|
+        (3, "Fizz")
+        (5, "Buzz")
+    |]
+
+let fizzbuzz number =
+    rules
+    |> Array.choose (fun (divisor, word) -> if number % divisor = 0 then Some(word) else None)
+    |> function
+        | [||] -> string number
+        | words -> String.concat "" words
+
+{ 1..100 }
+|> Seq.map fizzbuzz
+|> Seq.iter (printfn "%s")
---
+++ FSharp-Scripts/Hanoi.fsx
@@ -0,0 +1,35 @@
+﻿open System
+open System.Collections.Generic
+open System.Linq
+
+let range = 5
+let src = Stack<_>(Enumerable.Range(1, range).Reverse())
+let ancl = Stack<_>()
+let dest = Stack<_>()
+
+let printTower name tower =
+    let concatNumbers = Seq.map string >> String.concat ", "
+    let towerText = tower |> concatNumbers
+    printfn "%s = %s" name towerText
+
+let printAll() =
+    printTower "src " src
+    printTower "dest" dest
+    printTower "ancl" ancl
+    printfn ""
+
+let rec hanoi counter (src:Stack<_>) (dest:Stack<_>) (ancl:Stack<_>) =
+    match counter with
+    | 0 -> ()
+    | _ ->
+        let counter = counter - 1
+        hanoi counter src ancl dest
+        printAll()
+        src.Pop() |> dest.Push
+        hanoi counter ancl dest src
+
+hanoi range src dest ancl
+
+printAll()
+printfn "Destination:"
+printTower "dest" dest
