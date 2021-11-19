e06d592386eb0c09812e27383696265710781279
Michael Oyer <code@michaeloyer.com>
2021-11-18 21:42:30 -0500

    Add Tennis Game

---
+++ FSharp/TennisGame/TennisGame/Domain.fs
@@ -0,0 +1,46 @@
+﻿module TennisGame.Domain
+
+type Score =
+    | Love
+    | Fifteen
+    | Thirty
+    | Forty
+    | Advantage
+
+type Outcome =
+    | OnGoing of Score * Score
+    | GameReceiver
+    | GameServer
+
+type Match = Score * Score
+
+let startMatch() = Match(Love, Love)
+
+let scoreServer gameMatch =
+    match gameMatch with
+    | Forty, Forty -> OnGoing (Advantage, Forty)
+    | Advantage, _ -> GameServer
+    | _, Advantage -> OnGoing (Forty, Forty)
+    | Love, score -> OnGoing (Fifteen, score)
+    | Fifteen, score -> OnGoing (Thirty, score)
+    | Thirty, score -> OnGoing (Forty, score)
+    | Forty, _ -> GameServer
+
+let scoreReceiver gameMatch =
+    match gameMatch with
+    | _, Advantage -> GameReceiver
+    | Forty, Forty -> OnGoing (Forty, Advantage)
+    | Advantage, _ -> OnGoing (Forty, Forty)
+    | score, Love -> OnGoing (score, Fifteen)
+    | score, Fifteen -> OnGoing (score, Thirty)
+    | score, Thirty -> OnGoing (score, Forty)
+    | _, Forty -> GameReceiver
+
+let getScore outcome =
+    match outcome with
+    | GameServer -> "Victory Server"
+    | GameReceiver -> "Victory Receiver"
+    | OnGoing (Advantage, _) -> "Adv. Server"
+    | OnGoing (_, Advantage) -> "Adv. Receiver"
+    | OnGoing (Forty, Forty) -> "Deuce"
+    | OnGoing (score1, score2) -> $"{score1}-{score2}"
---
+++ FSharp/TennisGame/TennisGame/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open TennisGame.Domain
+
+[<EntryPoint>]
+let main _ =
+    let printScore = getScore >> (printfn "%s")
+
+    let readKey() =
+        let keyChar = Console.ReadKey() in
+            Console.Write("\b");
+        keyChar
+
+    let (|Key|) (keyInfo:ConsoleKeyInfo) =
+        Key(keyInfo.KeyChar)
+
+    let (|KeyText|) (keyInfo:ConsoleKeyInfo) =
+        match keyInfo.KeyChar with
+        | '0'
+        | char when char <= '3' && char <= '9' -> KeyText(keyInfo.KeyChar |> string)
+        | _ -> KeyText(keyInfo.Key |> string)
+
+    let rec scoreOfPlayerInput() =
+        match readKey() with
+        | Key('1') -> scoreServer
+        | Key('2') -> scoreReceiver
+        | key ->
+            printfn $"Invalid Key: {key.Key}"
+            scoreOfPlayerInput()
+
+    let rec playGame tennisMatch =
+        let scorePlayer = scoreOfPlayerInput()
+        match scorePlayer tennisMatch with
+        | GameServer -> printScore GameServer
+        | GameReceiver -> printScore GameReceiver
+        | OnGoing (serverScore, receiverScore) ->
+            printScore (OnGoing (serverScore, receiverScore))
+            playGame (serverScore, receiverScore)
+
+
+    printfn "Play tennis! Press '1' to score server, Press '2' to score receiver"
+    startMatch() |> playGame
+    0
